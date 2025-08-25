ece57c6ff5a123d99166422f6a7e524668a721f3
Sax Dhiut <mail@iyxeyl.me>
2025-08-26 00:19:18 +0400

    refactor: remove unused plugins from auth client configuration

--- src/lib/auth/auth-client.ts
+++ src/lib/auth/auth-client.ts
@@ -1,8 +1,6 @@
-import { passkeyClient, twoFactorClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 import { env } from "@/lib/env"
 
 export const authClient = createAuthClient({
-	baseURL: env.NEXT_PUBLIC_APP_URL,
-	plugins: [twoFactorClient(), passkeyClient()]
+	baseURL: env.NEXT_PUBLIC_APP_URL
 })

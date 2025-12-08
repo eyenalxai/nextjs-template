c9f8931fc70d7eeb4dc1902f597c469b22e438ed
Sax Dhiut <mail@iyxeyl.me>
2025-12-08 16:39:54 +0400

    fix: update extractDomain function to handle undefined URLs and adjust allowedDevOrigins based on environment

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 import type { NextConfig } from "next"
-import { env } from "@/lib/env"
 
-const extractDomain = (url: string) => {
+const extractDomain = (url: string | undefined) => {
+	if (!url) throw new Error("NEXT_PUBLIC_APP_URL is not set")
+
 	const urlObj = new URL(url)
 	return urlObj.hostname
 }
 
 const nextConfig: NextConfig = {
 	transpilePackages: ["geist"],
-	allowedDevOrigins: [extractDomain(env.NEXT_PUBLIC_APP_URL)]
+	allowedDevOrigins:
+		process.env.NODE_ENV !== "development"
+			? undefined
+			: [extractDomain(process.env.NEXT_PUBLIC_APP_URL)]
 }
 
 module.exports = nextConfig
